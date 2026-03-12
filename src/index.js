ecb53197a53e351274b7b9e6dbc6e8173878dc71
Yogeswar Reddy Rachamallu <yogeswarreddyrachamallu@Yogeswars-MacBook-Pro.local>
2026-03-13 01:09:06 +0530

    phase 4: rate limiting added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db');
 const routes = require('./routes');
+const rateLimiter = require('./rateLimiter');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(rateLimiter);
 connectDB();
 app.use('/', routes);
 const PORT = process.env.PORT || 3000;
---
+++ src/rateLimiter.js
@@ -0,0 +1,25 @@
+const redis = require('./redis');
+const rateLimiter = async (req, res, next) => {
+    try {
+        const ip = req.ip;
+        const key = `ratelimit:${ip}`;
+        const limit = 10;
+        const window = 60;
+
+        const requests = await redis.incr(key);
+        if (requests === 1) {
+            await redis.expire(key, window);
+        }
+        if (requests > limit) {
+            return res.status(429).json({
+                error: 'Too many requests, please try again after a minute'
+            })
+        }
+        next();
+    }
+    catch (error) {
+        next();
+    }
+};
+
+module.exports = rateLimiter;
