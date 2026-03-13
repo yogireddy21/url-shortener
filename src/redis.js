4937aa004bb5f97daa18c6c757bda34c9b9f492f
Yogeswar Reddy Rachamallu <yogeswarreddyrachamallu@Yogeswars-MacBook-Pro.local>
2026-03-13 14:53:44 +0530

    fix: redis url for railway

--- src/redis.js
+++ src/redis.js
@@ -1,19 +1,18 @@
 const Redis = require('ioredis');
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
-});
-
+const redis = process.env.REDIS_URL
+    ? new Redis(process.env.REDIS_URL)
+    : new Redis({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 6379,
+    });
 
 redis.on('connect', () => {
     console.log("Redis Connected");
 });
 
-
 redis.on('error', (error) => {
     console.log('Redis connection failed', error);
 });
-
 
 module.exports = redis;
